f8e7ebd0fa2854b207c43c83345655aea712fd25
Tukhtamurod Isroilov <inha07111999@gmail.com>
2020-02-27 16:15:13 +0500

    updated sorting

--- webpage/music.php
+++ webpage/music.php
@@ -21,22 +21,39 @@
       shuffle($music_list);
     }
   }
+  function merge_sort($my_array){
+	if(count($my_array) == 1 ) return $my_array;
+	$mid = count($my_array) / 2;
+    $left = array_slice($my_array, 0, $mid);
+    $right = array_slice($my_array, $mid);
+	$left = merge_sort($left);
+	$right = merge_sort($right);
+	return merge($left, $right);
+}
+function merge($left, $right){
+	$res = array();
+	while (count($left) > 0 && count($right) > 0){
+		if(filesize($left[0]) > filesize($right[0])){
+			$res[] = $right[0];
+			$right = array_slice($right , 1);
+		}else{
+			$res[] = $left[0];
+			$left = array_slice($left, 1);
+		}
+	}
+	while (count($left) > 0){
+		$res[] = $left[0];
+		$left = array_slice($left, 1);
+	}
+	while (count($right) > 0){
+		$res[] = $right[0];
+		$right = array_slice($right, 1);
+	}
+	return $res;
+}
   if(isset($_REQUEST["bysize"]) and !$is_playlist){
     if($_REQUEST["bysize"] == "on"){
-      $music_list_tmp = array();
-      for($i = 0; $i < count($music_list); $i++){
-        $largest = $music_list[0];
-        $index = 0;
-        for($j=1; $j < count($music_list); $j++){
-          if(filesize($music_list[$j]) > filesize($largest)){
-            $largest = $music_list[$j];
-            $index = $j;
-          }
-        }
-        \array_splice($music_list, $index, 1);
-        $music_list_tmp[] = $largest;
-      }
-      $music_list = $music_list_tmp;
+      $music_list = merge_sort($music_list);
     }
   }
   function fileSizeConverter($bytes){
